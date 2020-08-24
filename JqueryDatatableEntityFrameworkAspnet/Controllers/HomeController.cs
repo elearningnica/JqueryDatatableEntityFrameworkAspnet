3e9f686691f9f09edf9e7fa6e7fafe6ed9f197ef
Milton Navarro <milton.j.navarro.p@gmail.com>
2020-08-24 15:56:39 -0600

    Add project files.

---
+++ JqueryDatatableEntityFrameworkAspnet/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using JqueryDatatableEntityFrameworkAspnet.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryDatatableEntityFrameworkAspnet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> loadData()
+        {
+            var data = await getData();
+            return Json(new { data = data }, JsonRequestBehavior.AllowGet);
+        }
+
+        private async Task<List<Customer>> getData()
+        {
+            using (AdventureWorksDW2017Entities entities = new AdventureWorksDW2017Entities())
+            {
+                List<Customer> customerList = new List<Customer>();
+
+                var data = await entities.DimCustomer.Take(200).ToListAsync();
+
+                foreach (var item in data)
+                {
+                    Customer modelo = new Customer();
+
+                    modelo.Phone = item.Phone;
+                    modelo.CustomerKey = item.CustomerKey;
+                    modelo.FirstName = item.FirstName;
+                    modelo.LastName = item.LastName;
+                    modelo.EmailAddress = item.EmailAddress;
+
+                    customerList.Add(modelo);
+                }
+
+                return customerList;
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ JqueryDatatableEntityFrameworkAspnet/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JqueryDatatableEntityFrameworkAspnet.Models
+{
+    public class Customer
+    {
+        [Key]
+        [Display(Name = "Customer Key")]
+        public int CustomerKey { get; set; }
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Phone Number")]
+        public string Phone { get; set; }
+        [Display(Name = "Email Address")]
+        [StringLength(50)]
+        public string EmailAddress { get; set; }
+    }
+}
